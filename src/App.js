04707fe2f662610e1e8041b72a5088e02567484a
mahdiiithg <saternweb@gmail.com>
2024-01-12 09:43:09 -0800

    UPDATE

--- src/App.js
+++ src/App.js
@@ -253,7 +253,7 @@ import "./App.css";
 // }
 
 // export default App;
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Router from './routes/Routes';
 
@@ -281,8 +281,40 @@ import Router from './routes/Routes';
 
 function App() {
 
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setDeferredPrompt(event);
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
   return (
       <div>
+        <button onClick={handleInstallClick}>Install App</button>
         <Router />
       </div>
   );
