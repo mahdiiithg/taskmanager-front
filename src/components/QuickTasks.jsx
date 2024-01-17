ecfb2edfcdfe76a6506465deda3b31b73b94baa7
mahdiiithg <saternweb@gmail.com>
2024-01-17 03:30:50 -0800

    UPDATE

--- src/components/QuickTasks.jsx
+++ src/components/QuickTasks.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {motion} from 'framer-motion'
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
 
 const QuickTasks = (props) => {
   const { title, to, icon } = props;
   return (
-    <>
+    <motion.div
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
       <Link to={to}>
         <div className="flex gap-x-3 items-center">
           { icon }
@@ -17,7 +39,7 @@ const QuickTasks = (props) => {
           </div>
         </div>
       </Link>
-    </>
+    </motion.div>
   );
 };
 export default QuickTasks;
