efeab185c2cf79fd7b07d1de134428b7c6376f06
mahdiiithg <saternweb@gmail.com>
2024-01-20 04:26:20 -0800

    UPDATE

--- src/api/http.js
+++ src/api/http.js
@@ -7,7 +7,7 @@ const userToken = Cookies.get('userToken')
 const instance = axios.create({
   // baseURL: 'http://localhost:3000/',
   // baseURL: 'http://116.203.241.176:3000/',
-  baseURL: 'http://api.mahditahavorgar.com/',
+  baseURL: 'https://api.mahditahavorgar.com/',
   timeout: 100000,
   headers: {
     'content-type': 'application/json',
