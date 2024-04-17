bbeb8221bb35dad302c701244a011b82260c69b2
mahdiiithg <saternweb@gmail.com>
2024-04-17 21:15:02 +0330

    UPDATE

--- src/api/http.js
+++ src/api/http.js
@@ -6,8 +6,8 @@ const userToken = Cookies.get('userToken')
 // Axios instance
 const instance = axios.create({
   // baseURL: 'http://localhost:8080/api',
-  baseURL: 'http://128.140.34.190:8080/api',
-  // baseURL: 'https://mahditahavorgar.com/api',
+  // baseURL: 'http://128.140.34.190:8080/api',
+  baseURL: 'https://mahditahavorgar.com/api',
   // timeout: 100000,
   headers: {
     'content-type': 'application/json',
